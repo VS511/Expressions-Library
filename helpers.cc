672afacf335401c37246e19ae476df63da98783f
Vaibhav Singh <vaibhavsingh@Vaibhavs-MacBook-Air-2021.local>
2021-12-05 00:42:45 -0800

    Starter Code

---
+++ helpers.cc
@@ -0,0 +1,12 @@
+#include "Expr.h"
+#include "helpers.h"
+
+using namespace expr;
+
+shared_ptr<Num> expr::num(double val) {
+    return std::make_shared<Num>(val);
+}
+
+shared_ptr<Var> expr::var(string name) {
+    return std::make_shared<Var>(name);
+}
